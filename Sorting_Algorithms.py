60814b05da6193c51b1ff7c42a29be84d7f3ebd6
Popa Ioan-Ciprian <132392935+Ciprian1922@users.noreply.github.com>
2023-05-10 20:35:03 +0300

    Update Sorting_Algorithms.py

--- Sorting_Algorithms.py
+++ Sorting_Algorithms.py
@@ -1,12 +1,14 @@
 # Let's talk about how we can run this test by ourselves.
-# You can open the code inside a python compiler and what you will have to do 
-# is to insert the name of your output file (eg: timefor10elements.csv), 
+# You can open the code inside a python compiler(eg: PyCharm) and what you will have to do 
+# is to insert the name of your output file (eg: timefor10elements), 
 # insert the ammount of list to be sorted (larger number for a better precision(eg:100) if you 
 # compute the average of the running time for every sorting algorithm, but for more than 10.000 elements
 # you should reduce the preision (the lists to be sorted) so you get a faster running time of the algorithm),
 # and finally insert the size of the lists to be generated and sorted.
 # Every single sorting algorithm is sorting the same generated list so we can see better the difference in time
 # execution.
+# The output file will be generated in the same path as where the project will be situated, also the file will
+# auto-generate as a CSV file.
 # The colaboration for this code was done with two of my colleagues:
 # https://github.com/AlexandraStulianec and https://github.com/maxi-todo
 import random
